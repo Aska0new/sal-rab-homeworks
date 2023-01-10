ab8928a4eb631b5f07db45c20cae7d6995fe6997
A.Shevchenko <asiya.shevchenko@gmail.com>
2023-01-10 07:51:24 +0300

    task done v.1

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,19 +33,18 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+function sendRequest(name, phone, address {street, house, entrance, floor, flat}, goods, sum) {
+    let data = {client, goods: [], order: {}};
+    
+    data.client = `Клиент: ${name}, тел. ${phone}`;
 
     let countOfGoods = goods.length;
-
     for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+        data.goods.push(goods[i].title, goods[i].count);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
+    data.order.address = `Адрес доставки: ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    data.order.sum = sum;
 
     let jsonData = JSON.stringify(data);
 
